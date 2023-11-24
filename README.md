a4faa8e3f7b947d7c45b95ec31df2eba58e3e191
Vivek Tiwari <vt2174781@gmail.com>
2023-11-24 11:51:47 +0000

    (feat): integrate the 3d model to our application also add the mouse and key events for rotation

--- README.md
+++ README.md
@@ -8,3 +8,7 @@ Let's Write Down the main Steps :
 - npm install react-router-dom
 - npm install @react-three/fiber (for creating canvas)
 - npm install @react-three/drei
+- download the 3d models from sketchfab that you want to include in your application
+- also you can check the sample in <a href="https://gltf.pmnd.rs">gltf.pmnd.rs</a> here.
+- create a models folder and include the models you want and paste the models code that coming from the previous link.
+- for enable the 3d models animation we need to install the package called @react-spring/three
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,25 +1,57 @@
 import { Canvas } from '@react-three/fiber'
-import { Suspense } from 'react'
+import { Suspense, useState } from 'react'
 import Loader from '../components/Loader'
+import { Island } from '../models/Island'
 
 const Home = () => {
+  const [isRotating, setIsRotating] = useState(false);
+
+  const adjustIslandScreenSize = () => {
+    let screenScale = null;
+    let screenPosition = [0, -6.5, -43];
+    let screenRotation = [0.1, 2.5, 0];
+
+    if(window.innerWidth < 768){
+      screenScale = [40, 40, 40];
+    }
+    else
+    {
+      screenScale = [55, 55, 55];
+    }
+
+    return [ screenScale, screenPosition, screenRotation];
+  }
+
+  const [ islandScale, islandPosition, islandRotation ] = adjustIslandScreenSize(); 
+
   return (
     <section className="w-full h-screen relative">
         <Canvas 
-            className='w-full h-screen bg-transparent'
+            className={`w-full h-screen bg-transparent ${isRotating} ? 'cursor-grabbing' : 'cursor-grab'`}
             camera={{ near: 0.1, far: 1000 }}
         >
             {/* for loading purpose if canvas takes time to load it shows the loader */}
             <Suspense fallback={<Loader />}>
                 {/* create all type lights and select the one we want. We'll figure out next time. */}
-                <directionalLight />
-                <ambientLight />
-                <pointLight />
-                <spotLight />
-                <hemisphereLight />
+                <directionalLight position={[1,1,1]} intensity={2} />
+                <ambientLight intensity={0.5} />
+
+                {/* we don't need point light as it points in all direction */}
+                {/* <pointLight />  */}
+                {/* we don't need point light as it points in one direction */}
+                {/* <spotLight /> */}
+
+                <hemisphereLight skyColor="#b1e1ff" groundColor="#000000" intensity={1} />
 
                 {/* Let's add an object */}
                 {/* for object goto sketchflow website and choose what you like. */}
+                <Island 
+                  position = {islandPosition}
+                  scale = {islandScale}  
+                  rotation = {islandRotation}
+                  isRotating
+                  setIsRotating
+                />
 
             </Suspense>
         </Canvas>
