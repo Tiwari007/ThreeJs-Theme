caab8d12c680dbe231a508134257eae9f2772e45
Vivek Tiwari <vt2174781@gmail.com>
2023-12-13 06:59:43 +0000

    (feat): added the recent projects

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,9 +3,9 @@ import { NavLink } from "react-router-dom"
 const Navbar = () => {
   return (
     <header className="header flex justify-around pt-5">
-      <NavLink to="/" className="w-10 h-10 rounded-lg 
+      <NavLink to="/" className="w-20 h-10 rounded-lg 
       bg-white flex items-center justify-center font-bold shadow-md">
-        <p className="text-blue-500">🐲</p>
+        <p className="text-blue-500">🐲</p> Home
       </NavLink>
       <nav className="flex text-lg gap-7 font-medium">
         <NavLink to="/contact" className={({ isActive }) => isActive ? 'text-blue-500' : 'text-black'}>Contact</NavLink>
